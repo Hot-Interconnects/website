879d35757168c8b32fcf7f70b1f18eb55c763345
lykke-li <yukeli_ok@hotmail.com>
2025-04-23 13:28:57 -0700

    update update script

--- test_update.sh
+++ test_update.sh
@@ -2,7 +2,6 @@
 
 username=$1
 hostname=$2
-LOCAL_DIR=$3
 
-rsync -i -r ${LOCAL_DIR} ${username}@${hostname}:/home/${username}/
+rsync -i -r ./${hostname} ${username}@${hostname}:/home/${username}/
 
