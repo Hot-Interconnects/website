53eea7f81afa4e775f385c7ef79153501d762b6b
lykke-li <yukeli_ok@hotmail.com>
2026-02-03 11:58:19 -0800

    Update 2026 website

--- test_update.sh
+++ test_update.sh
@@ -5,6 +5,6 @@ hostname=$2
 
 rsync -i -r -P --exclude '2025' --timeout=120 -e "ssh -o ServerAliveInterval=60" ./${hostname} ${username}@${hostname}:/home/${username}/
 
-# git add -A
-# git commit -m "Update 2026 website"
-# git push origin main
+git add -A
+git commit -m "Update 2026 website"
+git push origin main
