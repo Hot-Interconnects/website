e3a7ec33fbb9176a7b920bde453bbbc5dea16a54
lykke-li <yukeli_ok@hotmail.com>
2026-02-13 14:11:08 -0800

    update script

--- test_update.sh
+++ test_update.sh
@@ -1,10 +1,23 @@
 #!/bin/bash
+set -e
 
 username=$1
 hostname=$2
 
-rsync -i -r -P --exclude '2025' --timeout=120 -e "ssh -o ServerAliveInterval=60" ./${hostname} ${username}@${hostname}:/home/${username}/
+# Fetch from GitHub
+git fetch origin || echo "Failed to fetch from the upstream"
 
-git add -A
-git commit -m "Update 2026 website"
+# Check that there are NO unstaged changes
+if ! git diff --quiet; then
+  echo "ERROR: There are unstaged changes. Please commit or stash them first."
+  exit 1
+fi
+
+# Rebase on top of the upstream branch
+git rebase origin/main || echo "Failed to rebase on top of Upstream"
+
+# Push to upstream
 git push origin main
+
+# Rsync website
+rsync -i -r -P --exclude '2025' --timeout=120 -e "ssh -o ServerAliveInterval=60" ./${hostname} ${username}@${hostname}:/home/${username}/
