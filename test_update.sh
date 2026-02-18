f18be120a5d332af804adb2c847c690cde81d8cb
lykke-li <yukeli_ok@hotmail.com>
2026-02-18 12:14:51 -0800

    update sync script for 2025

--- test_update.sh
+++ test_update.sh
@@ -1,10 +1,23 @@
 #!/bin/bash
+set -e
 
 username=$1
 hostname=$2
 
-rsync -i -r ./${hostname} ${username}@${hostname}:/home/${username}/
+# Fetch from GitHub
+git fetch origin || { echo "ERROR: Failed to fetch from the upstream"; exit 1; }
 
-git add *
-git commit -m "Update website"
-git push origin main
+# Check that there are NO unstaged changes
+if ! git diff --quiet; then
+  echo "ERROR: There are unstaged changes. Please commit or stash them first."
+  exit 1
+fi
+
+# Rebase on top of the upstream branch
+git rebase origin/2025 || { echo "ERROR: Failed to rebase on top of Upstream"; exit 1; }
+
+# Push to upstream
+git push origin 2025 || { echo "ERROR: Failed to push to upstream"; exit 1; }
+
+# Rsync website
+rsync -i -r -P --timeout=120 -e "ssh -o ServerAliveInterval=60" ./${hostname}/ ${username}@${hostname}:/home/${username}/${hostname}/2025/
